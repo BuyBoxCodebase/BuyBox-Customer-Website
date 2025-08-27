39494a833e1fc8ca619d386768feabe2affcdcfc
Dibs07 <banerjeedibs@gmail.com>
2025-08-27 18:37:39 +0530

    fixes

--- src/app/subcategory/[subcategoryId]/page.tsx
+++ src/app/subcategory/[subcategoryId]/page.tsx
@@ -57,7 +57,7 @@ function SubcategoryPageContent() {
       y: 0,
       opacity: 1,
       transition: {
-        type: "spring",
+        type: "spring" as const,
         stiffness: 300,
         damping: 24,
       },
