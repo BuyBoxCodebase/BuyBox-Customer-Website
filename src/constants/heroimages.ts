5740e67614acea482f59859dce3784b43827245a
Juan Lee Antunes <leeroyantunes14@icloud.com>
2026-09-15 20:04:52 +0200

    updated banner pics

--- src/constants/heroimages.ts
+++ src/constants/heroimages.ts
@@ -1,14 +1,26 @@
 export const images = [
   {
-    src: "https://i.postimg.cc/90RhrgjG/Whats-App-Image-2025-06-06-at-2-40-39-PM.jpg",
+    src: "https://i.postimg.cc/7L5QSdfy/Whats-App-Image-2026-09-15-at-19-28-22-(2).jpg",
     alt: "Image 1 description",
   },
   {
-    src: "https://i.postimg.cc/Pf9HBHmK/Whats-App-Image-2025-06-06-at-2-40-41-PM.jpg",
+    src: "https://i.postimg.cc/7hks8L9d/Whats-App-Image-2026-09-15-at-19-28-23-(1).jpg",
     alt: "Image 2 description",
   },
   {
-    src: "https://i.postimg.cc/nzLZtcRS/Whats-App-Image-2025-06-06-at-2-40-42-PM.jpg",
+    src: "https://i.postimg.cc/BbBhqGwH/Whats-App-Image-2026-09-15-at-19-28-23-(2).jpg",
     alt: "Image 3 description",
   },
+  {
+    src: "https://i.postimg.cc/tCzfbFLv/Whats-App-Image-2026-09-15-at-19-28-22-(1).jpg",
+    alt: "Image 4 description",
+  },
+  {
+    src: "https://i.postimg.cc/VsqZ9jLM/Whats-App-Image-2026-09-15-at-19-28-23.jpg",
+    alt: "Image 5 description",
+  },
+  {
+    src: "https://i.postimg.cc/CxQcM7J4/Whats-App-Image-2026-09-15-at-19-28-25.jpg",
+    alt: "Image 6 description",
+  }
 ];
